bc0b7667d86a140eabe7da154e5cb4dc2e9fbe8b
may10-codes <simonm487@gmail.com>
2024-02-26 20:44:46 +0300

    built ins to exit and get envs in shell

--- main.h
+++ main.h
@@ -6,12 +6,14 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #define MAX_ARGS 10
 
+int search_and_exe_commd(char *args[])
+char *path = getenv("PATH");
 
-
-
-
+size_t our_strlen(const char *src)
 
 
 #endif /* MAIN_H */
